6acf48729c8ac2857ccd7719b5391a4f0378285d
Swetha G <swethag09122002@gmail.com>
2026-05-05 18:50:38 +0530

    Cleanup: removed build files and added gitignore

--- build/CMakeFiles/pyTunning.dir/pyTunning_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/pyTunning.dir/pyTunning_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/amt-06/tunning/build/pyTunning_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/arm-linux-gnueabihf/include/stdc-predef.h
